c12c8454a7e5b5e1406757c1105342876a5bc10c
Kalashnikov Sergey <skalashnikov@timepath.ru>
2023-06-29 14:17:53 +0500

    Добавьте файлы проекта.

---
+++ TestLib/Figures/Circle.cs
@@ -0,0 +1,19 @@
+﻿using TestLib.Interface;
+
+namespace TestLib
+{
+    public class Circle : IFigure
+    {
+        private double radius;
+
+        Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
---
+++ TestLib/Figures/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using TestLib.Interface;
+
+namespace TestLib
+{
+    public class Triangle : IFigure
+    {
+        private double angle;
+
+        private double sideA, sideB;
+
+        Triangle(double sideA, double sideB, double angle)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.angle = angle;
+        }
+
+        public double GetArea()
+        {
+            if (angle <= 0 || angle >= 180)
+            {
+                throw new Exception("It is not Triangle");
+            }
+            if (angle == 90)
+            {
+                return 0.5 * sideA * sideB;
+            }
+            return 0.5 * sideA * sideB * Math.Sin(angle);
+        }
+    }
+}
